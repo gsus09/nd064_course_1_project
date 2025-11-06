8d1f92c86c75a88ee7e493b9f1ac7b50ab0f5988
Martin Thomas <martin.thomas@cariad.technology>
2025-11-06 10:06:18 +0100

    update app with logging / healthz / metrics

--- techtrends/app.py
+++ techtrends/app.py
@@ -1,28 +1,47 @@
 import sqlite3
-
+import logging
+import sys
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+
+# Configure logging to STDOUT with DEBUG level and timestamp
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel(logging.DEBUG)
+stdout_handler.setFormatter(logging.Formatter('%(levelname)s:%(filename)s:%(asctime)s, %(message)s',
+                                              datefmt='%d/%m/%Y, %H:%M:%S'))
+
+logger = logging.getLogger("app")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(stdout_handler)
+
+
+db_connection_count = 0
+
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global db_connection_count
+    db_connection_count += 1
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
     return connection
 
+
 # Function to get a post using its ID
 def get_post(post_id):
     connection = get_db_connection()
-    post = connection.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
+    post = connection.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
     connection.close()
     return post
+
 
 # Define the Flask application
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
-# Define the main route of the web application 
+
+# Define the main route of the web application
 @app.route('/')
 def index():
     connection = get_db_connection()
@@ -30,22 +49,28 @@ def index():
     connection.close()
     return render_template('index.html', posts=posts)
 
-# Define how each individual article is rendered 
+
+# Define how each individual article is rendered
 # If the post ID is not found a 404 page is shown
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+        logger.error(f"wrong post id {post_id} requested")
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        logger.info(f"post {post['title']} retrieved!")
+        return render_template('post.html', post=post)
+
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    logger.debug("About Us page retrieved")
     return render_template('about.html')
 
-# Define the post creation functionality 
+
+# Define the post creation functionality
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
@@ -56,15 +81,44 @@ def create():
             flash('Title is required!')
         else:
             connection = get_db_connection()
-            connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
+            connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
             connection.commit()
             connection.close()
+            logger.debug(f"New post '{title}' created!")
 
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
+
+@app.route('/metrics')
+def metrics():
+    connection = get_db_connection()
+    post_count = connection.execute('SELECT COUNT(*) FROM posts').fetchone()[0]
+    connection.close()
+    logger.debug('Metrics endpoint accessed.')
+    response = app.response_class(
+        response=json.dumps({
+            "db_connection_count": db_connection_count,
+            "post_count": post_count
+        }),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+@app.route('/healthz')
+def healthcheck():
+    logger.debug('Healthcheck endpoint accessed.')
+    response = app.response_class(
+            response=json.dumps({"result": "OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+    return response
+
+
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    app.run(host='0.0.0.0', port='3111')
